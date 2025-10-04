68ba809a41e5436de084ffd3ca2d4233fcabe0cf
Jennifer Kumar <67869018+jennykumar@users.noreply.github.com>
2025-10-04 21:57:01 +0530

    Delete monitor.h

--- monitor.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-
-bool isTemperatureInRange(float temperature);
-bool isPulseRateInRange(float pulseRate);
-bool isSpoInRange(float spo2);
-bool vitalsInRange(float temperature, float pulseRate, float spo2);
-void alert(const char* message);
-
